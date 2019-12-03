b59332bf60d518fef147e9d84b9abac5c8c9604e
MinexAutomation <minex.automation@gmail.com>
2019-12-02 16:53:03 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Votadinia.Default.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Votadinia.Default.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
