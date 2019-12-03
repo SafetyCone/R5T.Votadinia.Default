ac2b41bd2ebb54e42bb59905a62dc99f9b0ddc22
MinexAutomation <minex.automation@gmail.com>
2019-12-02 16:55:44 -0800

    Implemented the ICurrentUtcDateTimeProvider interface.

--- source/R5T.Votadinia.Default/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Votadinia.Default
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Votadinia.Default/Code/Services/Classes/CurrentUtcDateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Votadinia.Default
+{
+    public class CurrentUtcDateTimeProvider : ICurrentUtcDateTimeProvider
+    {
+        public DateTime GetCurrentUtcDateTime()
+        {
+            var currentUtcDateTime = DateTime.UtcNow;
+            return currentUtcDateTime;
+        }
+    }
+}
