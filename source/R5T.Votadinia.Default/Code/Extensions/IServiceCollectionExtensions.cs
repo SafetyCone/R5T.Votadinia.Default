4f2d96ef9375d3f3fba57e32c2d0a5dc1f8913d6
MinexAutomation <minex.automation@gmail.com>
2020-07-17 04:01:35 -0700

    Added IServiceCollection extensions.

---
+++ source/R5T.Votadinia.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Votadinia.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="CurrentUtcDateTimeProvider"/> implementation of <see cref="ICurrentUtcDateTimeProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddCurrentUtcDateTimeProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<ICurrentUtcDateTimeProvider, CurrentUtcDateTimeProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CurrentUtcDateTimeProvider"/> implementation of <see cref="ICurrentUtcDateTimeProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<ICurrentUtcDateTimeProvider> AddCurrentUtcDateTimeProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<ICurrentUtcDateTimeProvider>(() => services.AddCurrentUtcDateTimeProvider());
+            return serviceAction;
+        }
+    }
+}
