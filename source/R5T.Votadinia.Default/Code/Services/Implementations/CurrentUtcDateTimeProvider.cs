9e3e5e8c3e0c30c9c69a9b63e3dd150bee59a224
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:43 -0700

    Interim changes.

---
+++ source/R5T.Votadinia.Default/Code/Services/Implementations/CurrentUtcDateTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Votadinia.Default
+{
+    [ServiceImplementationMarker]
+    public class CurrentUtcDateTimeProvider : ICurrentUtcDateTimeProvider, IServiceImplementation
+    {
+        public DateTime GetCurrentUtcDateTime()
+        {
+            var currentUtcDateTime = DateTime.UtcNow;
+            return currentUtcDateTime;
+        }
+    }
+}
